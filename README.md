82753634eaab3c5b0a9b630f25fa99523e575ac0
Brian Magnuson <bmagnuso@amazon.com>
2021-12-10 09:14:31 -0500

    day 10

--- README.md
+++ README.md
@@ -3,10 +3,9 @@
 
 Solutions for [AOC 2021](https://adventofcode.com/2021/) in Haskell.
 
-* All solutions are in src/DayN.hs for both part 1 and 2 of each puzzle.
+* All solutions are in src/Aoc2021/DayN.hs for both part 1 and 2 of each puzzle.
 * Problem source is under input/dayN.txt
 
 ## To run:
-1. cabal repl
-2. Day\<day>.part1 or Day\<day>.part2
+1. cabal run
 
--- src/Aoc2021/Day10.hs
+++ src/Aoc2021/Day10.hs
@@ -8,9 +8,53 @@ import Aoc2021.AocUtils
 import Control.Applicative
 import Control.Lens
 import Data.Attoparsec.Text qualified as P
+import Data.List (sort)
 
--- input = getInput "input/day09.txt" parser
+input = getInput "input/day10.txt" parser
 
-part1 = 0
+parser = many (many (P.satisfy (P.inClass "[]{}()<>")) <* P.endOfLine)
 
-part2 = 0
+data Status
+  = Valid
+  | Invalid Int
+  | Incomplete Int
+  deriving (Show)
+
+$(makePrisms ''Status)
+
+scores = map (go []) input
+  where
+    go [] [] = Valid
+    go xs [] = Incomplete (foldl f 0 xs)
+      where
+        f a c = a * 5 + (s c)
+        s = \case
+          '(' -> 1
+          '[' -> 2
+          '{' -> 3
+          '<' -> 4
+    go [] (x : xs)
+      | x `elem` ("[(<{" :: String) = go [x] xs
+      | otherwise =
+          case x of
+            ']' -> Invalid 57
+            ')' -> Invalid 3
+            '>' -> Invalid 25137
+            '}' -> Invalid 1197
+    go s@(s' : ss) (x : xs)
+      | x `elem` ("[(<{" :: String) = go (x : s) xs
+      | otherwise =
+          case x of
+            ']' -> if s' == '[' then go ss xs else Invalid 57
+            ')' -> if s' == '(' then go ss xs else Invalid 3
+            '>' -> if s' == '<' then go ss xs else Invalid 25137
+            '}' -> if s' == '{' then go ss xs else Invalid 1197
+
+getScores :: Prism' Status Int -> [Status] -> [Int]
+getScores s l = l ^.. folded . filtered (has s) . s
+
+part1 = (sum . getScores _Invalid) scores
+
+part2 =
+  let l = (sort . getScores _Incomplete) scores
+   in l !! (length l `div` 2)
