bab00e5867b2c5eca4b49d2c46c28c47332a34ac
Brian Magnuson <bmagnuso@amazon.com>
2021-12-11 19:40:23 -0500

    day 11

--- src/Aoc2021/Day11.hs
+++ src/Aoc2021/Day11.hs
@@ -10,6 +10,7 @@ import Control.Lens
 import Data.Attoparsec.Text qualified as P
 import Data.Char (ord)
 import Data.List (findIndex)
+import Data.Maybe (fromJust)
 import Data.Vector qualified as V
 
 data Spot = Spot
@@ -27,28 +28,15 @@ input = getInput "input/day11.txt" parser
 
 parser = V.fromList <$> many ((V.fromList . map ((Spot False) . subtract 48 . ord)) <$> many P.digit <* P.endOfLine)
 
-neighbors (x, y) =
-  [ (x + 1, y),
-    (x - 1, y),
-    (x, y + 1),
-    (x, y - 1),
-    (x + 1, y + 1),
-    (x + 1, y - 1),
-    (x - 1, y + 1),
-    (x - 1, y - 1)
-  ]
+neighbors p = let l = [(+ 1), id, (subtract 1)] in [bimap a b p | a <- l, b <- l]
 
 allPts x = fmap fst $ x ^@.. itraversed <.> itraversed
-
-resetFlash x = x & traversed . traversed . flash .~ False
 
 step :: Grid -> Grid
 step g = go (resetFlash g) (allPts g)
   where
     go g [] = g
-    go g (p : ps) =
-      let (g', newPts) = incr g p
-       in go g' (ps ++ newPts)
+    go g (p : ps) = let (g', newPts) = incr g p in go g' (ps ++ newPts)
     incr :: Grid -> Pt -> (Grid, [Pt])
     incr g pt@(y, x) =
       case g ^? ix y . ix x of
@@ -56,11 +44,12 @@ step g = go (resetFlash g) (allPts g)
         Just (Spot True _) -> (g, [])
         Just (Spot False 9) -> (g & ix y . ix x .~ Spot True 0, neighbors pt)
         Just (Spot False n) -> (g & ix y . ix x .~ Spot False (n + 1), [])
-
-countFlashes = lengthOf (folded . folded . flash . filtered id)
+    resetFlash = traversed . traversed . flash .~ False
 
 part1 = sum . map countFlashes . take 101 $ (iterate step input)
+  where
+    countFlashes = lengthOf (folded . folded . flash . filtered id)
 
-part2 = findIndex allFlash (iterate step input)
+part2 = fromJust $ findIndex allFlash (iterate step input)
   where
     allFlash x = and (x ^.. traversed . traversed . flash)
